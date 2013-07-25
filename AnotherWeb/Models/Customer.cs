f7bb877a8bc7ff2640d91d9240b0ae5836f0f328
Hadi Hariri <mail@hadihariri.com>
2013-07-25 14:09:12 +0200

    more preps

---
+++ AnotherWeb/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Deployment.Internal;
+
+namespace AnotherWeb.Models
+{
+    public class Customer
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfLastContact { get; set; }
+        public int Priority { get; set; }
+
+        public Customer()
+        {
+            
+        }
+    }
+}
